6fb65ab4300a5562436183b49146dd4f3097ab4f
Kevin Hurley <kevinhurley23@gmail.com>
2023-09-21 09:05:55 -0400

    Remove Sanity references

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import sanity from "astro-sanity";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: "19f5gkij",
-      dataset: "production",
-      useCdn: true,
-      apiVersion: "v2021-03-25",
-    }),
-  ],
+  integrations: [],
 });
