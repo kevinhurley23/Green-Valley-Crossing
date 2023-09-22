44ba2aa6af241065b8509ecedca77417b69127b4
Kevin Hurley <kevinhurley23@gmail.com>
2023-09-22 11:16:20 -0400

    Api timeout and error handling

--- src/api.js
+++ src/api.js
@@ -1,29 +1,61 @@
 const endpoint = "https://gvc-cms.onrender.com/api/";
+
+function fetchWithTimeout(resource, options) {
+  const { timeout = 7000 } = options;
+
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(
+      () => reject(new Error("Request timed out")),
+      timeout
+    );
+
+    fetch(resource, options).then((response) => {
+      clearTimeout(timer);
+      resolve(response);
+    }, reject);
+  });
+}
+
 export async function getArticle(title) {
-  const response = await fetch(
-    endpoint + "articles?filters[title][$eqi]=" + title
-  );
-  const data = await response.json();
-  const content = data.data[0].attributes.content;
-  return content;
+  try {
+    const response = await fetchWithTimeout(
+      endpoint + "articles?filters[title][$eqi]=" + title,
+      { timeout: 15000 }
+    );
+    const data = await response.json();
+    const content = data.data[0].attributes.content;
+    return content;
+  } catch (error) {
+    console.error("Error fetching article:", error);
+    return `<p class="error-message">The requested resource could not be loaded<p>`;
+  }
 }
 
 export async function getBulletin() {
-  const response = await fetch(endpoint + "bulletins");
-  const data = await response.json();
+  try {
+    const response = await fetchWithTimeout(endpoint + "bulletins", {
+      timeout: 15000,
+    });
+    const data = await response.json();
 
-  const dateStr = data.data[0].attributes.date;
-  const date = new Date(dateStr);
-  const options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    timeZone: "UTC",
-  };
-  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(date);
+    const dateStr = data.data[0].attributes.date;
+    const date = new Date(dateStr);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "UTC",
+    };
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+      date
+    );
 
-  const serviceOrder = data.data[0].attributes.service_order;
-  const announcements = data.data[0].attributes.announcements;
+    const serviceOrder = data.data[0].attributes.service_order;
+    const announcements = data.data[0].attributes.announcements;
 
-  return [formattedDate, serviceOrder, announcements];
+    return [formattedDate, serviceOrder, announcements];
+  } catch (error) {
+    console.error("Error fetching bulletin:", error);
+    return `<p class="error-message">The requested resource could not be loaded<p>`;
+  }
 }
