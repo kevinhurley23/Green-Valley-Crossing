8a0595619405eaab892e342764ae85dd6d5bcda5
Kevin Hurley <kevinhurley23@gmail.com>
2024-01-07 15:42:52 -0500

    set base value

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kevinhurley23.github.io/",
-  // base: "/Green-Valley-Crossing",
+  base: "/Green-Valley-Crossing",
 });
